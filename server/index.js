05fee8e4c45d99874c745df7d19f62c6a51896bf
nevraka <nevrakaya@gmail.com>
2020-12-25 19:54:12 +0100

    Graphql explanation

--- server/index.js
+++ server/index.js
@@ -8,20 +8,64 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const data = {
+  portfolios: [
+    {
+      _id: '32dsa8d9',
+      title: 'Job in USA',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false,
+    },
+    {
+      _id: '5603okkd',
+      title: 'Job in Barcelona',
+      content: 'It was very suny experience',
+      jobTitle: 'Developer',
+      isCurrentlyEmployed: false,
+    },
+    {
+      _id: 'fsjlpa00',
+      title: 'Job in Germany',
+      content: 'It was very good',
+      jobTitle: 'Manager',
+      daysOfExperience: 30,
+      isCurrentlyEmployed: true,
+    },
+  ],
+};
+
 app.prepare().then(() => {
   const server = express();
 
   //Construct a schema using Graphql schema language
   const schema = buildSchema(`
-    type Query {
-      hello: String
+
+  type Porfolio{
+        _id:ID!
+        title:String
+        jobTitle:String,
+        content:String
+        daysOfExperience:Int,
+        isCurrentlyEmployed:Boolean
     }
+    type Query {
+        hello: String
+        portfolio: Porfolio
+        portfolios:[Porfolio]
+      }
   `);
 
   //The root provides a resolver for each API endpoint
   const root = {
     hello: () => {
       return 'Hello World!';
+    },
+    portfolio: () => {
+      return data.portfolios[0];
+    },
+    portfolio: () => {
+      return data.portfolios;
     },
   };
 
