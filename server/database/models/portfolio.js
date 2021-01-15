19bd26cae3659bdf15687deb5515204fe693c5d4
nevraka <nevrakaya@gmail.com>
2021-01-15 11:49:31 +0100

    added Portfolio Model

---
+++ server/database/models/portfolio.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const portfolioSchema = new Schema({
+  title: { type: String, require: true, maxlength: 128 },
+  company: { type: String, require: true, maxlength: 64 },
+  companyWebsite: { type: String, require: true, maxlength: 128 },
+  location: { type: String, require: true, maxlength: 128 },
+  jobTitle: { type: String, require: true },
+  description: { type: String, require: true },
+  startDate: { type: Date, require: true },
+  endDate: Date
+  createAt: { type: Date, default: Date.now },
+});
+
+
+module.exports=mongoose.model('Portfolio',portfolioSchema)
