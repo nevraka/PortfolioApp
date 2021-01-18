7fd413bb0b488d108eb0d56b89df1b04e7853b14
nevraka <nevrakaya@gmail.com>
2021-01-18 15:13:45 +0100

    Redirect function

--- pages/register.js
+++ pages/register.js
@@ -1,15 +1,28 @@
 import RegisterForm from '@/components/forms/RegisterForm';
+import { Mutation } from 'react-apollo';
+import { SIGN_UP } from '@/apollo/queries';
+import withApollo from '@/hoc/withApollo';
+import Redirect from '@/components/shared/Redirect';
+
 const Register = () => {
-  const register = (registerData) => {
-    alert(JSON.stringify(registerData));
-  };
   return (
     <>
       <div className="bwm-form mt-5">
         <div className="row">
           <div className="col-md-5 mx-auto">
             <h1 className="page-title">Register</h1>
-            <RegisterForm onSubmit={register} />
+            <Mutation mutation={SIGN_UP}>
+              {(signUpUser, { data, error }) => (
+                <>
+                  <RegisterForm
+                    onSubmit={(registerData) => {
+                      signUpUser({ variables: registerData });
+                    }}
+                  />
+                  {data && data.signUp && <Redirect to="/login" />}
+                </>
+              )}
+            </Mutation>
           </div>
         </div>
       </div>
@@ -17,4 +30,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default withApollo(Register);
