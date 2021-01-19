57c0783bfc49dcf7512b727cfe70dd26491c73db
nevraka <nevrakaya@gmail.com>
2021-01-19 21:26:02 +0100

    Handle errors on server

--- server/graphql/models/User.js
+++ server/graphql/models/User.js
@@ -1,39 +1,39 @@
-
-
-
 class User {
   constructor(model) {
     this.Model = model;
   }
 
-  signUp(signUpData) {
+  async signUp(signUpData) {
     if (signUpData.password !== signUpData.passwordConfirmation) {
       throw new Error('Password must be the same as confirmation password!');
     }
 
-    return this.Model.create(signUpData);
+    try {
+      return await this.Model.create(signUpData);
+    } catch (e) {
+      if (e.code && e.code === 11000) {
+        throw new Error('User with provided email already exists!');
+      }
+
+      throw e;
+    }
   }
 
   async signIn(signInData, ctx) {
     try {
       const user = await ctx.authenticate(signInData);
       return user;
-    } catch(error) {
+    } catch (error) {
       return error;
     }
   }
 
   signOut(ctx) {
     try {
-      // console.log('BEFORE LOGOUT-------------');
-      // console.log('is authenticated', ctx.isAuthenticated());
-      // console.log('user', ctx.getUser());
       ctx.logout();
-      // console.log('AFTER LOGOUT-------------');
-      // console.log('is authenticated', ctx.isAuthenticated());
-      // console.log('user', ctx.getUser());
+
       return true;
-    } catch(e) {
+    } catch (e) {
       return false;
     }
   }
