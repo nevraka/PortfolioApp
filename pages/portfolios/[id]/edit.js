c9d81e340b2f9ca8ff987eca36bf3c1e3de98886
nevraka <nevrakaya@gmail.com>
2021-02-08 14:31:16 +0100

    Update portfolio mutation

--- pages/portfolios/[id]/edit.js
+++ pages/portfolios/[id]/edit.js
@@ -3,12 +3,20 @@ import withApollo from '@/hoc/withApollo';
 import withAuth from '@/hoc/withAuth';
 import BaseLayout from '@/layouts/BaseLayout';
 import { useRouter } from 'next/router';
-import { useGetPortfolio } from '@/apollo/actions';
+import { useGetPortfolio, useUpdatePortfolio } from '@/apollo/actions';
 
 const PortfolioEdit = () => {
   const router = useRouter();
+  const [updatePortfolio, { error }] = useUpdatePortfolio();
   const { id } = router.query;
   const { data } = useGetPortfolio({ variables: { id } });
+
+  const errorMessage = (error) => {
+    return (
+      (error.graphQLErrors && error.graphQLErrors[0].message) ||
+      'Ooooops something went wrong...'
+    );
+  };
 
   return (
     <BaseLayout>
@@ -17,7 +25,15 @@ const PortfolioEdit = () => {
           <div className="col-md-5 mx-auto">
             <h1 className="page-title">Edit Portfolio</h1>
             {data && (
-              <PortfolioForm initialData={data.portfolio} onSubmit={() => {}} />
+              <PortfolioForm
+                initialData={data.portfolio}
+                onSubmit={(data) =>
+                  updatePortfolio({ variables: { id, ...data } })
+                }
+              />
+            )}
+            {error && (
+              <div className="alert alert-danger">{errorMessage(error)}</div>
             )}
           </div>
         </div>
